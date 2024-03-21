e6b841e39498a60e58d9ce64112db8e78b0d6062
Brokenaadi143 <164303603+Brokenaadi143@users.noreply.github.com>
2024-03-21 11:32:19 +0530

    Create 0_aadi.js

---
+++ modules/commands/0_aadi.js
@@ -0,0 +1,29 @@
+ const fs = require("fs");
+module.exports.config = {
+	name: "aadi",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "Arun", 
+	description: "no prefix",
+	commandCategory: "No command marks needed",
+	usages: "...",
+    cooldowns: 100, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("owner") ||
+     react.includes("aadi") || 
+react.includes("@ɭɭ-ʬIlıll Aadi Singh Ilıllʬ-ɭɭ")) {
+		var msg = {
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦🌸===『*★🌸===आदि बाबू✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ https://www.youtube.com/@Aadibrand-hh6gc ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽 \n 𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋 https://www.facebook.com/profile.php?id=100083551732772&mibextid=ZbWKwL`",
+				attachment: fs.createReadStream(__dirname + `/noprefix/kk1.jpg`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("📷", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
